954afdb2a0f03a673eb6fbf5e8fad573989f1b28
vinay-chindam <122191158+vinay-chindam@users.noreply.github.com>
2025-03-22 18:39:14 +0530

    Update StoreContext.jsx

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://tasty-go-backend.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
